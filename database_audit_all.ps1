55202e2aceac0c63c6505d380425859bcee01dc5
Mariusz Masewicz <orasup003@eos-ksi.pl>
2024-01-25 14:03:08 +0100

    automation scripts

---
+++ database_audit_all.ps1
@@ -0,0 +1,12 @@
+cd PATH_TO_RESULTS\results\DATABASE1_FOLDER
+Start-Process -FilePath "PATH_TO_SQLCL\sql.exe" -ArgumentList "user/password@database1_host:1521/SID1 @oracle_conf_audit.sql" -Wait
+
+cd PATH_TO_RESULTS\results\DATABASE2_FOLDER
+Start-Process -FilePath "PATH_TO_SQLCL\sql.exe" -ArgumentList "user/password@database1_host:1521/SID1 @oracle_conf_audit.sql" -Wait
+
+cd PATH_TO_RESULTS
+git add .
+$commit_message="Automatic audit of all databases: "+(Get-Date -format "yyyy-MM-dd")
+$commit_message
+git commit -m $commit_message
+git push
