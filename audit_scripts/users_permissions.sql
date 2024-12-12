e087d1c9572d7ac22024d059220fe482fa7387a0
Mariusz Masewicz <orasup003@eos-ksi.pl>
2024-12-12 11:51:23 +0100

    user_effective_privs

--- audit_scripts/users_permissions.sql
+++ audit_scripts/users_permissions.sql
@@ -36,6 +36,36 @@ spool audit_results/role_sys_privs.csv
 select * from role_sys_privs order by role,  privilege;
 spool off
 
+spool audit_results/user_effective_privs.csv
+select
+  level, lpad(' ', 2*level) || granted_role "User, his roles and privileges"
+from
+  (
+  /* THE USERS */
+    select 
+      null     grantee, 
+      username granted_role
+    from 
+      dba_users where upper(username) like ('PL%')
+  /* THE ROLES TO ROLES RELATIONS */ 
+  union
+    select 
+      grantee,
+      granted_role
+    from
+      dba_role_privs
+  /* THE ROLES TO PRIVILEGE RELATIONS */ 
+  union
+    select
+      grantee,
+      privilege
+    from
+      dba_sys_privs
+  )
+start with grantee is null
+connect by grantee = prior granted_role;
+spool off
+
 
 
 --select * from dba_credentials;
