e51567528f5e46c90f0b6b766fde350caaed2c78
Mariusz Masewicz <orasup003@eos-ksi.pl>
2025-01-07 23:32:16 +0100

    small changes  to execution process

--- database_audit_all.ps1
+++ database_audit_all.ps1
@@ -1,12 +1,12 @@
 cd PATH_TO_RESULTS\results\DATABASE1_FOLDER
-Start-Process -FilePath "PATH_TO_SQLCL\sql.exe" -ArgumentList "user/password@database1_host:1521/SID1 @oracle_conf_audit.sql" -Wait
+Start-Process -FilePath "PATH_TO_SQLCL\sql.exe" -ArgumentList "user/password@database1_host:1521/SID1 @oracle_conf_audit.sql" -Wait #-NoNewWindow
 
 cd PATH_TO_RESULTS\results\DATABASE2_FOLDER
-Start-Process -FilePath "PATH_TO_SQLCL\sql.exe" -ArgumentList "user/password@database1_host:1521/SID1 @oracle_conf_audit.sql" -Wait
+Start-Process -FilePath "PATH_TO_SQLCL\sql.exe" -ArgumentList "user/password@database1_host:1521/SID1 @oracle_conf_audit.sql" -Wait #-NoNewWindow
 
 cd PATH_TO_RESULTS
 git add .
 $commit_message="Automatic audit of all databases: "+(Get-Date -format "yyyy-MM-dd HH:mm:ss")
-$commit_message
+#$commit_message
 git commit -m $commit_message
 git push
