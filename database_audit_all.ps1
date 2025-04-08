b588139c2c3dd5c37fb060c753ecdb88b2c69086
Mariusz Masewicz <Mariusz.Masewicz@summ-it.pl>
2025-04-08 16:26:51 +0200

    automacic pull before start

--- database_audit_all.ps1
+++ database_audit_all.ps1
@@ -2,15 +2,20 @@ $CurrentDir=$pwd.Path
 $SQLPath="PATH_TO_SQLCL\sql.exe"
 $ResultsPath="PATH_TO_RESULTS\results"
 $AuditScript="oracle_conf_audit.sql"
-$AdditionalOptions="-Wait #-NoNewWindow"
+$OracleAuditScripts="path_to_local_Oracle_Audit_repository"
+
+
+Get-Date -format "yyyy-MM-dd HH:mm:ss"
+cd $OracleAuditScripts
+git pull
 
 cd $ResultsPath\database1_host1
 $pwd.Path
-Start-Process -FilePath $SQLPath -ArgumentList "user1/password1@database1_host:1521/SID1 @$AuditScript" $AdditionalOptions"
+Start-Process -FilePath $SQLPath -ArgumentList "user1/password1@database1_host:1521/SID1 @$AuditScript" -Wait #-NoNewWindow
 
 cd $ResultsPath\database2_host2
 $pwd.Path
-Start-Process -FilePath $SQLPath -ArgumentList "user2/password2@database2_host:1521/SID2 @$AuditScript# $AdditionalOptions"
+Start-Process -FilePath $SQLPath -ArgumentList "user2/password2@database2_host:1521/SID2 @$AuditScript#" -Wait #-NoNewWindow
 
 cd $ResultsPath
 git add .
@@ -19,3 +24,4 @@ $commit_message="Automatic audit of all databases: "+(Get-Date -format "yyyy-MM-
 git commit -m $commit_message
 git push
 cd $CurrentDir
+Get-Date -format "yyyy-MM-dd HH:mm:ss"
